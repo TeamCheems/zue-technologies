9f0d397eb9e9418e9a175ce51faf6bc0ff1f2804
Kingswin <kingswin5549@gmail.com>
2021-08-13 22:14:53 +0530

    img size fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="vh-80" id="home"> 
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="row w-75">
-                    <div className="col-12 col-md-7 text-center text-md-start">
+                    <div className="col-12 col-md-7 text-center text-md-start my-auto">
                         <h1 className="font-weight-bold"> Your right stop for </h1>
                         <h1 style={{ fontWeight: 'bolder'}}> industrial automation solutions.</h1>
                         <button className="btn btn-dark my-4 btn-lg">Get in touch</button>

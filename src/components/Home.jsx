71846c688d9aef70e7b01e538327bad365522e0e
Kingswin <kingswin5549@gmail.com>
2021-08-16 21:28:54 +0530

    Main page Size increses.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@
 
 export default function Home() {
     return (
-        <div className="vh-80" id="home"> 
+        <div className="vh-100" id="home"> 
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="row w-75">
                     <div className="col-12 col-md-7 text-center text-md-start my-auto">
-                        <h1 className="font-weight-bold"> Your right stop for </h1>
-                        <h1 style={{ fontWeight: 'bolder'}}> industrial automation solutions.</h1>
-                        <button className="btn btn-dark my-4 btn-lg">Get in touch</button>
+                        <h1 style={{fontSize: '50px'}}> Your Right Stop For </h1>
+                        <h1 style={{ fontWeight: 'bolder',fontSize: '70px' }}> Industrial Automation Solutions.</h1>
+                        <button className="btn btn-dark my-4 btn-lg" style={{ fontSize: '30px'}}>Get in touch</button>
                     </div>
                     <div className="col-12 col-md-5 text-center"><img className="pic" src="frontpic.png"/></div>
                 </div>

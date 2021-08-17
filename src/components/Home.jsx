257c99f5ea769d05f04786db3b1663c3a9aa7376
Joekin Roche <joekinroches@gmail.com>
2021-08-17 13:20:05 +0530

    Altered size of image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,11 @@ export default function Home() {
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="row w-75">
                     <div className="col-12 col-md-7 text-center text-md-start my-auto">
-                        <h1 style={{fontSize: '50px'}}> Your Right Stop For </h1>
-                        <h1 style={{ fontWeight: 'bolder',fontSize: '70px' }}> Industrial Automation Solutions.</h1>
+                        <h1 className="mt-5 title-small"> Your Right Stop For </h1>
+                        <h1 className="title-large" > Industrial Automation Solutions.</h1>
                         <button className="btn btn-dark my-4 btn-lg" style={{ fontSize: '30px'}}>Get in touch</button>
                     </div>
-                    <div className="col-12 col-md-5 text-center"><img className="pic" src="frontpic.png"/></div>
+                    <div className="col-12 col-md-5 text-center mt-5"><img className="pic" src="frontpic.png"/></div>
                 </div>
             </div>
         </div>

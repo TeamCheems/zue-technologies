a2ebbdbd5e1d2f1070148a5c8a9d7985b6ddd365
Kingswin <kingswin5549@gmail.com>
2021-08-21 01:46:22 +0530

    Typewriter fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect'; 
 
-
-
-    // const [state] = useState({
-    //     title : "",
-    //     titleTwo : "",
-    // });
-    
-
 export default function Home(){
     return (
         <div className="vh-100" id="home"> 
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="row w-75">
-                    <div className="col-12 col-md-7 text-center text-md-start">
-                        <h1 className="font-weight-bold"> Your right stop for </h1>
-                            <h1  className="titleLarge" style={{ fontWeight: 'bolder'}}>Industrial Automation Solutions.</h1>
-                            <h1  className="titleLarge" style={{ fontWeight: 'bolder'}}>Student Project</h1>
-                            <button className="btn btn-dark my-4">Get in touch</button>
-                        </div>
-                        <div className="col-12 col-md-5 text-center mt-5"><img className="pic" src="frontpic.png"/></div>
-                        <div className= "text">
+                    <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12 padding_zero">
+                        <h1 className="font-weight-bold" style={{fontSize:40}}> Your Right Stop For </h1>
+                            <h1  className="title-Large" style={{ fontWeight: 'bolder', fontSize:50}}>
                             <Typewriter className = ""
                             options = {{
                                 autoStart: true,
                                 loop : true,
-                                delay: 40,
+                                delay: 100,
                                 strings:[
                                     "Industrial Automation Solutions.",
-                                    "Student Project",
-                                    "King is great KittuDas"
+                                    "Students Project Work Guidance.",
+                                    "Develope Your Great Ideas."
                                 ],
                             }}/>
+                            </h1>
+                            <button className="btn-lg btn-dark my-5">Get in touch</button>
+                        </div>
+                        <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12 padding_zero"><img className="pic" src="frontpic.png"/></div>
+                        <div className= "text">
                     </div>
                 </div>
             </div>

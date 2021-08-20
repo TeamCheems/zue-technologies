6022618f64ccaf10a9a7d33d04c58ae4fd9890b5
Praveen <praveenrijal.23it@licet.ac.in>
2021-08-21 02:15:36 +0530

    btn fun

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,24 +7,24 @@ export default function Home(){
             <div className="h-100 d-flex justify-content-center align-items-center">
                 <div className="row w-75">
                     <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12 padding_zero">
-                        <h1 className="font-weight-bold" style={{fontSize:40}}> Your Right Stop For </h1>
-                            <h1  className="title-Large" style={{ fontWeight: 'bolder', fontSize:50}}>
+                        <h1 className="font-weight-bold" style={{fontSize:40}}> Your Right Stop to </h1>
+                            <h1  className="title-Large" style={{ fontWeight: 'bolder', fontSize:45}}>
                             <Typewriter className = ""
                             options = {{
-                                autoStart: true,
-                                loop : true,
-                                delay: 100,
-                                strings:[
-                                    "Industrial Automation Solutions.",
-                                    "Students Project Work Guidance.",
-                                    "Develop Your Great Ideas."
-                                ],
-                            }}/>
+                            autoStart: true,
+                            loop : true,
+                            delay: 150,
+                            strings:[
+                                "Industrial Automation Solutions.",
+                                "Students Project Work Guidance.",
+                                "Develop Your Great Ideas."
+                            ],
+                        }}/>
                             </h1>
                             <button className="btn-lg btn-dark my-5">Get in touch</button>
-                        </div>
-                        <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12 padding_zero"><img className="pic" src="frontpic.png"/></div>
-                        <div className= "text">
+                    </div>
+                    <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 padding_zero">
+                        <img className="pic " src="frontpic.png"/>
                     </div>
                 </div>
             </div>

f97e78cbcfe5794168f43be410e239311afff2c5
Kingswin <kingswin5549@gmail.com>
2021-08-23 20:01:27 +0530

    class added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="vh-100 bg-dark" id="contact"> 
+        <div className="font vh-100 bg-dark" id="contact"> 
             <h1> Contact </h1>
         </div>
     )

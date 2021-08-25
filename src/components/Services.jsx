0902604bdbf0ab89765e9590e6127365379f4715
Kingswin <kingswin5549@gmail.com>
2021-08-25 20:48:38 +0530

    services edited

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div id="services"> 
             <div className="p-5">
-                <div className="text-center pt-5 h2" style={{ fontWeight: 'bolder'}}>Our Expertise</div>
+                <div className="text-center pt-5 h2 " style={{ fontWeight: 'bolder'}}>Our Expertise</div>
                 <div className="text-center pt-3 pb-3 h6" style={{ fontWeight: '500'}}>
                     Right from app development to scaling your organization, we have all the capabilities you need
                 </div>
@@ -13,7 +13,7 @@ export default function About() {
                     <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
                         <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
                         <div className="about-title my-3">
-                            App Development
+                            Project for Educational Institute
                         </div>
                         <div className="about-content">
                             Build a secure, future-proof application through the latest technologies and enhance your user experience
@@ -25,7 +25,7 @@ export default function About() {
                     <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
                         <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
                         <div className="about-title my-3">
-                            App Development
+                            Project Guidance for Students
                         </div>
                         <div className="about-content">
                             Build a secure, future-proof application through the latest technologies and enhance your user experience
@@ -37,7 +37,43 @@ export default function About() {
                     <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
                         <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
                         <div className="about-title my-3">
-                            App Development
+                            Project for Public User
+                        </div>
+                        <div className="about-content">
+                            Build a secure, future-proof application through the latest technologies and enhance your user experience
+                        </div>
+                        <div className="my-4">
+                            Read more <a href="#" style={{fontSize: '10px', color: 'black'}}><FontAwesomeIcon icon={faArrowRight} /></a> 
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
+                        <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
+                        <div className="about-title my-3">
+                            Industrial Automation 
+                        </div>
+                        <div className="about-content">
+                            Build a secure, future-proof application through the latest technologies and enhance your user experience
+                        </div>
+                        <div className="my-4">
+                            Read more <a href="#" style={{fontSize: '10px', color: 'black'}}><FontAwesomeIcon icon={faArrowRight} /></a> 
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
+                        <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
+                        <div className="about-title my-3">
+                            Special Purpose Machine
+                        </div>
+                        <div className="about-content">
+                            Build a secure, future-proof application through the latest technologies and enhance your user experience
+                        </div>
+                        <div className="my-4">
+                            Read more <a href="#" style={{fontSize: '10px', color: 'black'}}><FontAwesomeIcon icon={faArrowRight} /></a> 
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-3 m-3 bg-light rounded p-4">
+                        <a href="#" style={{fontSize: '60px', color: 'black'}}><FontAwesomeIcon icon={faIndustry} /></a> 
+                        <div className="about-title my-3">
+                            Residential Automation
                         </div>
                         <div className="about-content">
                             Build a secure, future-proof application through the latest technologies and enhance your user experience

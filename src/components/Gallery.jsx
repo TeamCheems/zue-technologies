db7e768cfff495599077b6b81572802ee5222e53
Kingswin <kingswin5549@gmail.com>
2021-08-25 21:59:01 +0530

    gallery img added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,49 @@
 export default function Bothub() {
     return (
         <div className="m-5" id="gallery"> 
-            Gallery
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+
+    <img
+      src="favicon.ico"
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt=""
+    />
+  </div>
+</div>
         </div>
         
     )

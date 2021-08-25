1bd6067159893a469de21b2c8ba0096902ecd0af
dhivya910 <dhivyadharshni.23it@licet.ac.in>
2021-08-25 13:56:21 +0530

    About page done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,81 @@
-
 import React from "react";
 
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
-          </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
-            </a>
+      <div className="h-100 d-flex justify-content-center align-items-center">
+        <div className="row w-75">
+        <div className="text-center pt-5 h2" style={{ fontWeight: 'bolder'}}>About Us</div>
+          <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12 padding_zero">
+          
+            <div className="col justify-content-center my-3">
+              <div className="row">
+                    <div className="col-12 col-md-6 m-3 bg-light rounded p-4">
+                        <div className="about-content">
+                        We are a group of engineering enthusiasts, featuring an exclusive range of utility projects for all 
+                        industrial automation purposes for handling different processes and machinery in the industry. 
+                        </div>
+                        <div className="my-4">
+                            
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 m-3 bg-light rounded p-4">
+                        
+                        <div className="about-content">
+                        Since the advent in 2019, we are committed to developing, building, and maintaining quality 
+                        automation systems that help you reach your goals.
+                        </div>
+                        <div className="my-4">
+          
+                        </div>
+                    </div>
+                    </div>
+                    <div className="row">
+                   
+                    <div className="col-12 col-md-6 m-3 bg-light rounded p-4">
+                        <div className="about-content">
+                        Today’s ever-changing technology offers new and innovative ways to approach automation and control issues. 
+                        To take advantage of these new technologies, you need a partner who understands how to apply them to your 
+                        needs.
+                        </div>
+                        <div className="my-4">
+                            
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 m-3 bg-light rounded p-4">
+                        <div className="about-content">
+                        No matter how unique your production needs, we are committed to the development of 
+                        control systems that will reduce your cost of doing business. 
+                        </div>
+                        <div className="my-4">
+                            
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+          </div>
+          <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 padding_zero">
+            <img className="picture " src="./favicon.ico" />
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="pic"
-            alt="hero"
-            src="./favicon.ico" 
-          />
-        </div>
       </div>
+      {/* <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          
+          <div className="row w-75">
+          
+                   
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img
+                className="pic"
+                alt="hero"
+                src="./favicon.ico" 
+                />
+                </div>
+              </div>
+            </div>
+          </div> */}
     </section>
   );
 }

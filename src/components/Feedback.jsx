6ac8ca9e798e3321a192e65b579127e02d0a44f2
Praveen <praveenrijal.23it@licet.ac.in>
2021-08-29 16:35:26 +0530

    Testimonial

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -10,14 +10,16 @@ export default function Feedback(){
             <div data-aos="fade-up" data-aos-duration="2000">
                 <div className="row d-flex justify-content-center">
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                    <img className="img-fluid" src="img1.jpg"/>
-                      <div className="about-content">
+                      <div className="img-fluid  col-md-3">                    
+                        <img className="img-fluid " src="img1.jpg"/>
+                        </div>
+                      <div className="about-content ">
                         <strong>Sanjeeva Reddy</strong> <br></br>
                         We were one among the fir effort to give us a quality time. Our house is well constructed and fulfills our requirements in terms of both space and cost as well. We are extremely happy with our new spacious house at Arputhalaya. We sincerely feel that it would be prudent to buy a house at Arputhalaya, since with the escalating land prices owning an independent villa in Chennai would be very difficult in future.
                       </div>
                   </div> 
-                  <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid" src="img1.jpg"/>
+                  <div className="col-md-5 m-3 bg-light rounded p-4">
+                  <img className="img-fluid col-md-3" src="img1.jpg"/>
                       <div className="about-content">
                       <strong>Stephen Prabu</strong> <br></br>
                       The houses in Arputhalaya are constructed in a well designed plan with all high quality modern facilities with an elegant look. All this are with in the budget and it is moneys worth. The project is actually  located in the midst of major educational institutions,residential townships and high-end gated communities. Prefer a home in Arputhalaya for a happy and peaceful living.
@@ -26,19 +28,17 @@ export default function Feedback(){
                 </div>
                 <div className="row d-flex justify-content-center">
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid" src="img1.jpg"/>
+                  <img className="img-fluid col-md-3" src="img1.jpg"/>
                       <div className="about-content">
                       <strong>Mrs & Mr.Ivan Nathan</strong> <br></br>
                       We wanted a duplex villa and Arputhalaya helped us fulfill our dreams. We are very happy with the elevation anedule and we are very glad that we decided to invest in a house in Arputhalaya.
                       </div>
                   </div>
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid" src="img1.jpg"/>
+                  <img className="img-fluid col-md-3" src="img1.jpg"/>
                       <div className="about-content">
                       <strong>Mr Praveen</strong> <br></br>
-                      
-                         The examining or considering again of something in order to decide iflaya helped us fulfill our dreams. We are v changes are necessary.
-                        
+                          The examining or considering again of something in order to decide iflaya helped us fulfill our dreams. We are v changes are necessary.
                       </div>
                   </div>
                 </div>

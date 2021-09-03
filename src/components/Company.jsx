9e3f50271f3cd119c40fe2662718baae6f3cf200
Kingswin <kingswin5549@gmail.com>
2021-09-03 19:29:16 +0530

    Scrolling part

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -3,5 +3,13 @@ import React from "react";
 export default function Client() {
   return (
    
-        <section className="bg-dark vh-20 p-5 mb-5"></section>
+        <section className="bg-dark vh-30 p-5 mb-5 align-items-center" data-aos="fade-up" data-aos-duration="2000">
+          <marquee behavior="scroll" direction="left" hspace="10%">
+            <img src="favicon.ico" width="300" height="200" alt="Company" style={{padding:20}} /> 
+            <img src="favicon.ico" width="300" height="200" alt="Company" style={{padding:20}} /> 
+            <img src="favicon.ico" width="300" height="200" alt="Company" style={{padding:20}} />
+            <img src="favicon.ico" width="300" height="200" alt="Company" style={{padding:20}} />
+            <img src="favicon.ico" width="300" height="200" alt="Company" style={{padding:20}} />
+          </marquee>
+        </section>
          )}

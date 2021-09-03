d73dbee22ab07578be091e2f75c999ee866cf50e
Kingswin <kingswin5549@gmail.com>
2021-09-03 13:03:04 +0530

    aligned testimonial

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -10,15 +10,17 @@ export default function Feedback(){
             <div data-aos="fade-up" data-aos-duration="2000">
                 <div className="row d-flex justify-content-center">
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                      <div className="img-fluid  col-md-3">                    
+                      <div className="row img-fluid col-md-3">                    
                         <img className="img-fluid " src="img1.jpg"/>
                         </div>
                       <div className="about-content ">
                         <strong>Sanjeeva Reddy</strong> <br></br>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                      </div>
                   </div> 
-                  <div className="col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid col-md-3" src="img1.jpg"/>
+                  <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
+                    <div className=" row img-fluid col-md-3">
+                  <img className="img-fluid" src="img1.jpg"/>
+                  </div>
                       <div className="about-content">
                       <strong>Stephen Prabu</strong> <br></br>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                      </div>
@@ -26,13 +28,17 @@ export default function Feedback(){
                 </div>
                 <div className="row d-flex justify-content-center">
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid col-md-3" src="img1.jpg"/>
+                    <div className="row img-fluid col-md-3">
+                  <img className="img-fluid" src="img1.jpg"/>
+                  </div>
                       <div className="about-content">
                       <strong>Mrs & Mr.Ivan Nathan</strong> <br></br>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                      </div>
                   </div>
                   <div className="col-12 col-md-5 m-3 bg-light rounded p-4">
-                  <img className="img-fluid col-md-3" src="img1.jpg"/>
+                    <div className="row img-fluid col-md-3">
+                  <img className="img-fluid" src="img1.jpg"/>
+                  </div>
                       <div className="about-content">
                       <strong>Mr Praveen</strong> <br></br>
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                      </div>

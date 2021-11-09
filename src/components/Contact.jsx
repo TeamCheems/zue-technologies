ca0a8a4fcb0ea919bce43de07c4ce6123c120399
Kingswin <kingswin5549@gmail.com>
2021-11-09 23:05:04 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,22 +10,22 @@ export default function Home() {
                 <h1>Contact</h1>
                     <div class="row">
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                            <h6><FontAwesomeIcon icon={faBuilding} /><i class="fa fa-building"></i>     Zue Technologies</h6>
+                            <h6><FontAwesomeIcon icon={faBuilding} /><i class="fa fa-building mx-2"></i>Zue Technologies</h6>
                             <p class="text-justify">Build your Ideas.</p>
                         </div>
 
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                            <h6><FontAwesomeIcon icon={faPhoneAlt} /><i class="fa fa-phonealt"></i>   Phone</h6>
+                            <h6><FontAwesomeIcon icon={faPhoneAlt} /><i class="fa fa-phonealt mx-2"></i>Phone</h6>
                             <p>8870795518</p>
                         </div>
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                        <h6><FontAwesomeIcon icon={faEnvelope} /><i class="fa fa-envelope"></i>  Email</h6>
+                        <h6><FontAwesomeIcon icon={faEnvelope} /><i class="fa fa-envelope mx-2"></i>Email</h6>
                             <p>zuetechnologies@gmail.com</p>
                             </div>  
                     </div>
                     <div class="row">
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                        <h6><FontAwesomeIcon icon={faAddressCard} /><i class="fa fa-addresscard"></i>   Address</h6>
+                        <h6><FontAwesomeIcon icon={faAddressCard} /><i class="fa fa-addresscard mx-2"></i>Address</h6>
                             <p>9B, Anboli Nagar,</p> 
                             <p>Chitra Nagar, Saravanampatti, </p>
                             <p>Coimbatore,</p>
@@ -33,7 +33,7 @@ export default function Home() {
                         
                         </div>
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                        <h6><FontAwesomeIcon icon={faMapMarkerAlt} /><i class="fa fa-mapmakeralt"></i>   Location</h6>
+                        <h6><FontAwesomeIcon icon={faMapMarkerAlt} /><i class="fa fa-mapmakeralt mx-2"></i>Location</h6>
                         <div>
          
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15662.534226813983!2d77.0045315!3d11.0660762!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2a87110308fe547!2sZue%20technologies!5e0!3m2!1sen!2sin!4v1632845844510!5m2!1sen!2sin"></iframe>
@@ -42,7 +42,7 @@ export default function Home() {
                         </div>
                         
                         <div class="col-lg-4 col-md-6 mb-4 justify-content-center flex-column text-justify">
-                        <h6><FontAwesomeIcon icon={faPlus} /><i class="fa fa-plus"></i>   Follow us</h6><br/>
+                        <h6><FontAwesomeIcon icon={faPlus} /><i class="fa fa-plus mx-2"></i>Follow us</h6><br/>
                         <ul class="social-icons">
                             <li><a class="facebook" href="https://www.facebook.com/zuetechnologies.zuetechnologies"><FontAwesomeIcon icon={faFacebook} /><i class="fa fa-facebook"></i></a></li>
                             <li><a class="youtube" href=" https://youtube.com/channel/UCt1c-lSSaqtnH_JJRB_1Trw"><FontAwesomeIcon icon={faYoutube} /><i class="fa fa-youtube"></i></a></li>
